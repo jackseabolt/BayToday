a04b5f655207b3d347d529bf722f7cd0738ae009
John Seabolt <johnseabolt@Johns-MacBook-Pro.local>
2016-10-26 15:56:08 -0700

    eineoh

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,15 @@
+class PagesController < ApplicationController
+
+	def policy
+
+	end
+
+	def about
+
+	end
+
+	def election_2016
+
+	end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   resources :posts
 
+  get "pages/policy" => "pages#policy"
+  get "pages/contact" => "pages#contact"
+  get "pages/about" => "pages#about"
+  get "pages/election2016" => "pages#election2016"
+
   root "posts#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
