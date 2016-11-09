de5a574e8110d1bc7bae6c01af62ac518f5cc2c6
John Seabolt <johnseabolt@Johns-MacBook-Pro.local>
2016-11-09 15:00:26 -0800

    fixed contact form

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,8 @@ Rails.application.configure do
     logger.formatter = config.log_formatter
     config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
-
+  
+  config.action_mailer.default_url_options = { :host => "http://http://baytoday.herokuapp.com/" }
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
