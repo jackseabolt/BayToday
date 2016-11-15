755631fbcceb6d9a6b395513431f259d2dd59a18
John Seabolt <johnseabolt@Johns-MacBook-Pro.local>
2016-11-15 09:48:17 -0800

    hopefuly working

--- config/application.rb
+++ config/application.rb
@@ -1,18 +1,5 @@
 
 
-config.action_mailer.smtp_settings = {
-  address: "smtp.gmail.com",
-  port: 587,
-  domain: "<example.tld>",
-  user_name: "jackseabolt",
-  password: "7Highland",
-  authentication: :plain,
-  enable_starttls_auto: true
-}
-
-config.action_mailer.default_url_options = {
-  host: "yourdomain.tld"
-}
 
 require_relative 'boot'
 
